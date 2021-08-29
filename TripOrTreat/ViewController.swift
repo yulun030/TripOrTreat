4fd4d1778460fd71ffd7897ebeec1e8cab754492
Ellen Huang <mana199708@gmail.com>
2021-08-29 16:42:34 +0800

    Initial Commit

---
+++ TripOrTreat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripOrTreat
+//
+//  Created by 綸綸 on 2021/8/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
